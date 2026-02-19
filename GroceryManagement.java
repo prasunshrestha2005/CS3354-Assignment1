ead35eecc360fb967270bb889630ef759ccc1c8f
prasunshrestha2005 <prasunshrestha2005@gmail.com>
2026-02-19 13:16:00 -0600

    Update GroceryManagement.java

--- GroceryManagement.java
+++ GroceryManagement.java
@@ -1,7 +1,40 @@
+/**
+ * GroceryManagement
+ *
+ * Base structure with inventory display feature.
+ */
+
 public class GroceryManagement {
 
-    public static void main(String[] args) {
-        System.out.println("Grocery Management System");
+    /**
+     * Prints all non-null inventory items.
+     */
+    public static void printInventory(String[] names, double[] prices, int[] stocks) {
+
+        for (int i = 0; i < names.length; i++) {
+
+            if (names[i] != null) {
+
+                System.out.println(
+                        "Item: " + names[i] +
+                        " | Price: $" + prices[i] +
+                        " | Stock: " + stocks[i]
+                );
+
+            } else {
+                // required if-else
+            }
+        }
     }
 
+    public static void main(String[] args) {
+
+        String[] itemNames = new String[10];
+        double[] itemPrices = new double[10];
+        int[] itemStocks = new int[10];
+
+        itemNames[0] = "Milk";
+        itemPrices[0] = 2.99;
+        itemStocks[0] = 10;
+    }
 }
